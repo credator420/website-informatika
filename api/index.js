184a5e03d5f65162bec79517d91d046ae4787f3a
credator420 <jaksadav@gmail.com>
2026-04-09 20:38:33 +0200

    Auto-sync: 2026-04-09

--- api/index.js
+++ api/index.js
@@ -11,7 +11,11 @@ dotenv.config();
 const app = express();
 
 // 1. MUST BE FIRST
-app.use(cors()); 
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // 2. MUST BE SECOND
 app.use(express.json()); 
